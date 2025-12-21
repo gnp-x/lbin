80664a73bb640a12d06da0917072a6e9b22683ab
Greg P. <88697977+gnp-x@users.noreply.github.com>
2025-12-21 16:21:48 -0600

    Complete mvp

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 use actix_files::Files;
-use actix_web::post;
-use std::io::Error;
-
 use actix_multipart::form::{
     MultipartForm,
     tempfile::{TempFile, TempFileConfig},
 };
-use actix_web::{App, HttpServer, Responder};
-use uuid::Uuid;
+use actix_web::{App, Error, HttpResponse, HttpServer, Responder, post};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
+use hightower_naming::generate_random_name;
+
 #[derive(Debug, MultipartForm)]
 struct UploadForm {
     #[multipart(rename = "file", limit = "5MB")]
-    files: Vec<TempFile>,
+    file: TempFile,
 }
 
-const PORT: u16 = 3696;
-const HOST: &str = "localhost";
+const PORT: &'static str = env!("port");
+const HOST: &'static str = env!("host");
+const AUTH: &'static str = env!("auth");
 
 #[post("/")]
 async fn save_files(
     MultipartForm(form): MultipartForm<UploadForm>,
+    cred: BearerAuth,
 ) -> Result<impl Responder, Error> {
     let mut file = String::new();
-    for f in form.files {
+    if cred.token() == AUTH {
+        let f = form.file;
         let path: String;
         if let Some(s) = f.file_name {
-            let filename = Uuid::new_v4().to_string().split_off(24);
+            let filename = generate_random_name(None);
             let split: Vec<_> = s.split(".").collect();
             if split.len() > 1 {
                 let extension = split[split.len() - 1];
@@ -40,22 +42,24 @@ async fn save_files(
             f.file.persist(&path).ok();
         };
         file.push('\n');
+        let url = format!("http://{}:{}/{}", HOST, PORT, file);
+        Ok(HttpResponse::Ok().body(url))
+    } else {
+        Ok(HttpResponse::Unauthorized().body("Invalid auth token.\n"))
     }
-    Ok(format!("http://{}:{}/{}", HOST, PORT, file))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::fs::create_dir_all("./tmp")?;
     println!("Starting up file server on port {HOST}:{PORT}");
-
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
             .app_data(TempFileConfig::default().directory("./tmp"))
             .service(save_files)
             .service(Files::new("/", "./tmp").index_file("index.html"))
     })
-    .bind((HOST, PORT))?
+    .bind((HOST, PORT.parse().expect("Error converting port to number")))?
     .run()
     .await
 }
